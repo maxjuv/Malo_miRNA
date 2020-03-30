958ee7f6e2189f395f15a58095897c993489d357
Maxime Juventin <maximejuventin@macbook-air-de-maxime.home>
2020-03-30 12:17:12 +0200

    from mean to weighted average

--- average_baseline_recovery.py
+++ average_baseline_recovery.py
@@ -1,7 +1,7 @@
 from configuration import *
 from select_mice_cata_Malo import get_mice
 
-def average_baseline(spectrum_method = 'somno'):
+def average_baseline_spectrum(spectrum_method = 'somno'):
     for group in ['Control', 'DCR-HCRT']:
         dirname = excel_dir + '{}/power_spectrum'.format(group)
         if group == 'Control':
@@ -9,23 +9,47 @@ def average_baseline(spectrum_method = 'somno'):
         elif group == 'DCR-HCRT':
             states = ['w', 'n', 'r', 'a']
         for state in states :
-            for period in ['light', 'dark']:
-                df1 = pd.read_excel(dirname+'/spectrum_{}_{}_bl1_{}.xlsx'.format(spectrum_method, state, period), index_col =0)
-                df2 = pd.read_excel(dirname+'/spectrum_{}_{}_bl2_{}.xlsx'.format(spectrum_method, state, period), index_col =0)
-                mice1 = df1.index
-                mice2 = df2.index
-                freqs = df1.columns[4:].to_list()
-                df = pd.DataFrame(index = get_mice(group), columns = ['ref_values'] + freqs )
-                for ind in range(mice1.size) :
-                    mouse1 = mice1[ind]
-                    mouse2 = mice2[ind]
-                    mouse = mouse1[:5]
-                    df.at[mouse, 'ref_values'] = (df1.at[mouse1, 'ref_values']+ df2.at[mouse2, 'ref_values'])/2
-                    transient = np.vstack((df1.loc[mouse1, freqs].values, df2.loc[mouse2, freqs].values))
-                    df.loc[mouse,freqs] = np.nanmean(transient, axis =0)
-                df.to_excel(dirname + '/spectrum_{}_{}_{}_baseline_{}.xlsx'.format(group,spectrum_method, state, period))
+            period = 'light'
+            df1 = pd.read_excel(dirname+'/spectrum_{}_{}_bl1_{}.xlsx'.format(spectrum_method, state, period), index_col =0)
+            df2 = pd.read_excel(dirname+'/spectrum_{}_{}_bl2_{}.xlsx'.format(spectrum_method, state, period), index_col =0)
+            mice1 = df1.index
+            mice2 = df2.index
+            freqs = df1.columns[4:].to_list()
+            df = pd.DataFrame(index = get_mice(group), columns = freqs )
+            for ind in range(mice1.size) :
+                mouse1 = mice1[ind]
+                mouse2 = mice2[ind]
+                mouse = mouse1[:5]
+                values1 = df1.loc[mouse1, freqs].values
+                values2 = df2.loc[mouse2, freqs].values
+                if sum(np.isnan(np.array(values1, dtype = 'float64'))) == values1.size  or sum(np.isnan(np.array(values2, dtype = 'float64'))) == values2.size :
+                    weighted_mean = np.nansum((values1,values2), axis = 0)
+                else :
+                    weighted_mean = (12/24)*values1 + (12/24)*values2
+                df.loc[mouse,freqs] = weighted_mean
+            df.to_excel(dirname + '/spectrum_{}_{}_{}_baseline_{}.xlsx'.format(group, spectrum_method, state, period))
+            #
+            period = 'dark'
+            df1 = pd.read_excel(dirname+'/spectrum_{}_{}_bl1_{}.xlsx'.format(spectrum_method, state, period), index_col =0)
+            df2 = pd.read_excel(dirname+'/spectrum_{}_{}_bl2_{}.xlsx'.format(spectrum_method, state, period), index_col =0)
+            mice1 = df1.index
+            mice2 = df2.index
+            freqs = df1.columns[4:].to_list()
+            df = pd.DataFrame(index = get_mice(group), columns = ['ref_values'] + freqs )
+            for ind in range(mice1.size) :
+                mouse1 = mice1[ind]
+                mouse2 = mice2[ind]
+                mouse = mouse1[:5]
+                values1 = df1.loc[mouse1, freqs].values
+                values2 = df2.loc[mouse2, freqs].values
+                if sum(np.isnan(np.array(values1, dtype = 'float64'))) == values1.size  or sum(np.isnan(np.array(values2, dtype = 'float64'))) == values2.size :
+                    weighted_mean = np.nansum((values1,values2), axis = 0)
+                else :
+                    weighted_mean = (12/24)*values1 + (12/24)*values2
+                df.loc[mouse,freqs] = weighted_mean
+            df.to_excel(dirname + '/spectrum_{}_{}_{}_baseline_{}.xlsx'.format(group, spectrum_method, state, period))
 
-def average_recovery(spectrum_method = 'somno'):
+def average_recovery_spectrum(spectrum_method = 'somno'):
     for group in ['Control', 'DCR-HCRT']:
         dirname = excel_dir + '{}/power_spectrum'.format(group)
         if group == 'Control':
@@ -33,26 +57,121 @@ def average_recovery(spectrum_method = 'somno'):
         elif group == 'DCR-HCRT':
             states = ['w', 'n', 'r', 'a']
         for state in states :
-            for period in ['light', 'dark']:
-                df1 = pd.read_excel(dirname+'/spectrum_{}_{}_sd_{}.xlsx'.format(spectrum_method, state, period), index_col =0)
-                df2 = pd.read_excel(dirname+'/spectrum_{}_{}_r1_{}.xlsx'.format(spectrum_method, state, period), index_col =0)
-                mice1 = df1.index
-                mice2 = df2.index
-                freqs = df1.columns[4:].to_list()
-                df = pd.DataFrame(index = get_mice(group), columns = ['ref_values'] + freqs )
-                for ind in range(mice1.size) :
-                    mouse1 = mice1[ind]
-                    mouse2 = mice2[ind]
-                    mouse = mouse1[:5]
-                    df.at[mouse, 'ref_values'] = (df1.at[mouse1, 'ref_values']+ df2.at[mouse2, 'ref_values'])/2
-                    transient = np.vstack((df1.loc[mouse1, freqs].values, df2.loc[mouse2, freqs].values))
-                    df.loc[mouse,freqs] = np.nanmean(transient, axis =0)
-                df.to_excel(dirname + '/spectrum_{}_{}_{}_recovery_{}.xlsx'.format(group, spectrum_method, state, period))
+            period = 'light'
+            df1 = pd.read_excel(dirname+'/spectrum_{}_{}_sd_{}.xlsx'.format(spectrum_method, state, period), index_col =0)
+            df2 = pd.read_excel(dirname+'/spectrum_{}_{}_r1_{}.xlsx'.format(spectrum_method, state, period), index_col =0)
+            mice1 = df1.index
+            mice2 = df2.index
+            freqs = df1.columns[4:].to_list()
+            df = pd.DataFrame(index = get_mice(group), columns = freqs )
+            for ind in range(mice1.size) :
+                mouse1 = mice1[ind]
+                mouse2 = mice2[ind]
+                mouse = mouse1[:5]
+                values1 = df1.loc[mouse1, freqs].values
+                values2 = df2.loc[mouse2, freqs].values
+                if sum(np.isnan(np.array(values1, dtype = 'float64'))) == values1.size  or sum(np.isnan(np.array(values2, dtype = 'float64'))) == values2.size :
+                    weighted_mean = np.nansum((values1,values2), axis = 0)
+                else :
+                    weighted_mean = (6/18)*values1 + (12/18)*values2
+                df.loc[mouse,freqs] = weighted_mean
+            df.to_excel(dirname + '/spectrum_{}_{}_{}_recovery_{}.xlsx'.format(group, spectrum_method, state, period))
+            #
+            period = 'dark'
+            df1 = pd.read_excel(dirname+'/spectrum_{}_{}_sd_{}.xlsx'.format(spectrum_method, state, period), index_col =0)
+            df2 = pd.read_excel(dirname+'/spectrum_{}_{}_r1_{}.xlsx'.format(spectrum_method, state, period), index_col =0)
+            mice1 = df1.index
+            mice2 = df2.index
+            freqs = df1.columns[4:].to_list()
+            df = pd.DataFrame(index = get_mice(group), columns =  freqs )
+            for ind in range(mice1.size) :
+                mouse1 = mice1[ind]
+                mouse2 = mice2[ind]
+                mouse = mouse1[:5]
+                values1 = df1.loc[mouse1, freqs].values
+                values2 = df2.loc[mouse2, freqs].values
+                if sum(np.isnan(np.array(values1, dtype = 'float64'))) == values1.size  or sum(np.isnan(np.array(values2, dtype = 'float64'))) == values2.size :
+                    weighted_mean = np.nansum((values1,values2), axis = 0)
+                else :
+                    weighted_mean = (12/24)*values1 + (12/24)*values2
+                df.loc[mouse,freqs] = weighted_mean
+            df.to_excel(dirname + '/spectrum_{}_{}_{}_recovery_{}.xlsx'.format(group, spectrum_method, state, period))
+            #
+            #
 
+def average_baseline_fragmentation():
+    encoding_to_state = {'a':'cata', 'w':'wake', 'n':'nrem', 'r':'rem'}
+    for group in ['Control', 'DCR-HCRT']:
+        dirname = excel_dir + '{}/sleep_fragmentation'.format(group)
+        if group == 'Control':
+            encoding = ['w', 'n', 'r']
+        elif group == 'DCR-HCRT':
+            encoding = ['w', 'n', 'r', 'a']
+        for e in encoding :
+            state = encoding_to_state[e]
+            period = 'light'
+            df1 = pd.read_excel(dirname+'/{}/{}_{}1.xlsx'.format(state, state, period), index_col =0)
+            df2 = pd.read_excel(dirname+'/{}/{}_{}2.xlsx'.format(state, state, period), index_col =0)
+            df = pd.DataFrame(index = df1.index, columns = df1.columns)
+            mouse = df1.index.to_list()
+            bouts = df1.columns.to_list()
+            for mouse in mouse :
+                values1 = df1.loc[mouse, bouts].values
+                values2 = df2.loc[mouse, bouts].values
+                weighted_mean = (12/24)*values1 + (12/24)*values2
+                df.loc[mouse, bouts] = weighted_mean
+            df.to_excel(dirname+'/{}/{}_baseline_{}.xlsx'.format(state, state, period))
+            period = 'dark'
+            df1 = pd.read_excel(dirname+'/{}/{}_{}1.xlsx'.format(state, state, period), index_col =0)
+            df2 = pd.read_excel(dirname+'/{}/{}_{}2.xlsx'.format(state, state, period), index_col =0)
+            df = pd.DataFrame(index = df1.index, columns = df1.columns)
+            mouse = df1.index.to_list()
+            bouts = df1.columns.to_list()
+            for mouse in mouse :
+                values1 = df1.loc[mouse, bouts].values
+                values2 = df2.loc[mouse, bouts].values
+                weighted_mean = (12/24)*values1 + (12/24)*values2
+                df.loc[mouse, bouts] = weighted_mean
+            df.to_excel(dirname+'/{}/{}_baseline_{}.xlsx'.format(state, state, period))
 
+def average_recovery_fragmentation():
+    encoding_to_state = {'a':'cata', 'w':'wake', 'n':'nrem', 'r':'rem'}
+    for group in ['Control', 'DCR-HCRT']:
+        dirname = excel_dir + '{}/sleep_fragmentation'.format(group)
+        if group == 'Control':
+            encoding = ['w', 'n', 'r']
+        elif group == 'DCR-HCRT':
+            encoding = ['w', 'n', 'r', 'a']
+        for e in encoding :
+            state = encoding_to_state[e]
+            period = 'light'
+            df1 = pd.read_excel(dirname+'/{}/{}_{}3.xlsx'.format(state, state, period), index_col =0)
+            df2 = pd.read_excel(dirname+'/{}/{}_{}4.xlsx'.format(state, state, period), index_col =0)
+            df = pd.DataFrame(index = df1.index, columns = df1.columns)
+            mouse = df1.index.to_list()
+            bouts = df1.columns.to_list()
+            for mouse in mouse :
+                values1 = df1.loc[mouse, bouts].values
+                values2 = df2.loc[mouse, bouts].values
+                weighted_mean = (6/18)*values1 + (12/18)*values2
+                df.loc[mouse, bouts] = weighted_mean
+            df.to_excel(dirname+'/{}/{}_recovery_{}.xlsx'.format(state, state, period))
+            period = 'dark'
+            df1 = pd.read_excel(dirname+'/{}/{}_{}3.xlsx'.format(state, state, period), index_col =0)
+            df2 = pd.read_excel(dirname+'/{}/{}_{}4.xlsx'.format(state, state, period), index_col =0)
+            df = pd.DataFrame(index = df1.index, columns = df1.columns)
+            mouse = df1.index.to_list()
+            bouts = df1.columns.to_list()
+            for mouse in mouse :
+                values1 = df1.loc[mouse, bouts].values
+                values2 = df2.loc[mouse, bouts].values
+                weighted_mean = (12/24)*values1 + (12/24)*values2
 
-
+                df.loc[mouse, bouts] = weighted_mean
+            df.to_excel(dirname+'/{}/{}_recovery_{}.xlsx'.format(state, state, period))
 
 if __name__ == '__main__':
-    average_baseline()
-    average_recovery()
+    average_baseline_spectrum()
+    average_recovery_spectrum()
+    average_baseline_fragmentation()
+    average_recovery_fragmentation()

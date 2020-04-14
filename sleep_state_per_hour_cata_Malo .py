9e5df028d3dc8f825cc72f23e3913082054f796d
Maxime Juventin <maximejuventin@macbook-air-de-maxime.home>
2020-04-14 18:55:22 +0200

    add REM latency function

--- sleep_state_per_hour_cata_Malo .py
+++ sleep_state_per_hour_cata_Malo .py
@@ -168,124 +168,124 @@ def plot_sleep_state_accross_time(group):
 
     plt.show()
 
-
-def plot_compare_sleep_state_accross_time(control ='Control', test ='DCR_HCRT'):
-    # data_dir = 'C:/Users/maxime.juventin/Desktop/scripts_ML/data/'
-    path_control = precompute_dir +  '/' +control+'/sleep_by_epoch.nc'
-    path_cre = precompute_dir + '/'+ test+'/sleep_by_epoch.nc'
-    ds_control = xr.open_dataset(path_control)
-    ds_cre = xr.open_dataset(path_cre)
-
-    number_of_epochs = ds_cre['wake_all_mice'].shape[0]
-    epoch_duration = 4
-    duration_in_hours = int(number_of_epochs*epoch_duration/3600)
-    wake_control = ds_control['wake_all_mice'].to_pandas()
-    nrem_control = ds_control['nrem_all_mice'].to_pandas()
-    rem_control = ds_control['rem_all_mice'].to_pandas()
-    cata_control = ds_control['cata_all_mice'].to_pandas()
-
-    wake_cre = ds_cre['wake_all_mice'].to_pandas()
-    nrem_cre = ds_cre['nrem_all_mice'].to_pandas()
-    rem_cre = ds_cre['rem_all_mice'].to_pandas()
-    cata_cre = ds_cre['cata_all_mice'].to_pandas()
-
-    time = int(1) # window to look at in HOUR. CAUTION must be a multiple of 100
-    window = int(time * 3600 / epoch_duration)
-    wake_by_time_control = []
-    rem_by_time_control = []
-    nrem_by_time_control = []
-    cata_by_time_control = []
-
-    wake_by_time_cre = []
-    rem_by_time_cre = []
-    nrem_by_time_cre = []
-    cata_by_time_cre = []
-
-    for h, i in enumerate(np.arange(0, number_of_epochs, window)):
-
-        wake_by_time_control.append(wake_control[i : i+window].sum())
-        rem_by_time_control.append(rem_control[i : i+window].sum())
-        nrem_by_time_control.append(nrem_control[i : i+window].sum())
-        cata_by_time_control.append(cata_control[i : i+window].sum())
-
-        wake_by_time_cre.append(wake_cre[i : i+window].sum())
-        rem_by_time_cre.append(rem_cre[i : i+window].sum())
-        nrem_by_time_cre.append(nrem_cre[i : i+window].sum())
-        cata_by_time_cre.append(cata_cre[i : i+window].sum())
-    wake_by_time_control = pd.concat(wake_by_time_control, axis = 1)
-    cata_by_time_control = pd.concat(cata_by_time_control, axis = 1)
-    rem_by_time_control = pd.concat(rem_by_time_control, axis = 1)
-    nrem_by_time_control = pd.concat(nrem_by_time_control, axis = 1)
-    wake_by_time_cre = pd.concat(wake_by_time_cre, axis = 1)
-    rem_by_time_cre = pd.concat(rem_by_time_cre, axis = 1)
-    nrem_by_time_cre = pd.concat(nrem_by_time_cre, axis = 1)
-    cata_by_time_cre = pd.concat(cata_by_time_cre, axis = 1)
-
-    # print(wake_by_time.shape)
-    fig, ax = plt.subplots(nrows=3)
-    times = np.arange(0, duration_in_hours)
-    plot  = np.arange(3)
-    controls = [wake_by_time_control, rem_by_time_control, nrem_by_time_control, cata_by_time_control]
-    cres = [wake_by_time_cre, rem_by_time_cre, nrem_by_time_cre, cata_by_time_cre]
-    # colors = [ 'black', 'red', 'blue']
-    # colors = [ 'black', 'green', 'black', 'green', 'black', 'green']
-    labels = ['control', 'control' ,'control', 'control', 'DCR', 'DCR', 'DCR' 'DCR']
-
-    for i, data_control, data_cre, label in zip(plot, controls, cres,labels) :
-        m = data_control.mean(axis=0)*epoch_duration/60
-        s =data_control.std(axis=0)*epoch_duration/60
-        ax[i].plot(times, m, color = 'black', label = label)
-        ax[i].fill_between(times, m-s, m+s , color = 'black', alpha = .2,)
-        m = data_cre.mean(axis=0)*epoch_duration/60
-        s =data_cre.std(axis=0)*epoch_duration/60
-        ax[i].plot(times, m, color = 'green', label = label)
-        ax[i].fill_between(times, m-s, m+s , color = 'green', alpha = .2)
-
-
-    nights = { 'dark0' : [0, 4],
-                 'dark1' : [16, 28],
-                 'dark2' : [40, 52],
-                 'dark3':[64, 76],
-                 'dark4':[88, 100]}
-
-    for night in nights :
-        h1, h2 = nights[night][0], nights[night][1]
-        ax[0].axvspan(h1, h2 , color = 'black', alpha = .3)
-        ax[1].axvspan(h1, h2 , color = 'black', alpha = .3)
-        ax[2].axvspan(h1, h2 , color = 'black', alpha = .3)
-        ax[3].axvspan(h1, h2 , color = 'black', alpha = .3)
-    height0_1, height0_2 =ax[0].get_ylim()[1]*.95, ax[0].get_ylim()[1]*1.05
-    height1_1, height1_2 = ax[1].get_ylim()[1]*.95, ax[1].get_ylim()[1]*1.05
-    height2_1, height2_2 = ax[2].get_ylim()[1]*.95, ax[2].get_ylim()[1]*1.05
-    height3_1, height3_2 = ax[3].get_ylim()[1]*.95, ax[3].get_ylim()[1]*1.05
-    for x1, x2 in zip([0,28,76], [4,52,100]):
-        lim = 110
-        x1 += 5
-        x2 +=5
-        ax[0].axhspan(height0_1, height0_2, xmin =x1/lim, xmax = x2/lim, fill = False, edgecolor = 'black', hatch = '////')
-        ax[1].axhspan(height1_1, height1_2, xmin =x1/lim, xmax = x2/lim, fill = False, edgecolor = 'black', hatch = '////')
-        ax[2].axhspan(height2_1, height2_2, xmin =x1/lim, xmax = x2/lim, fill = False, edgecolor = 'black', hatch = '////')
-        ax[2].axhspan(height3_1, height3_2, xmin =x1/lim, xmax = x2/lim, fill = False, edgecolor = 'black', hatch = '////')
-    x1 = 52 + 5
-    x2 = 58 +5
-    ax[0].axhspan(height0_1, height0_2, xmin =x1/lim, xmax = x2/lim, color = 'red')
-    ax[1].axhspan(height1_1, height1_2, xmin =x1/lim, xmax = x2/lim, color = 'red')
-    ax[2].axhspan(height2_1, height2_2, xmin =x1/lim, xmax = x2/lim, color = 'red')
-    ax[3].axhspan(height3_1, height3_2, xmin =x1/lim, xmax = x2/lim, color = 'red')
-        # ax[0].axhspan(i,j , color = 'black', alpha = .3)
-        # ax[1].axhspan(i,j , color = 'black', alpha = .3)
-        # ax[2].axhspan(i,j , color = 'black', alpha = .3)
-    ax[0].set_title('Sleep state amount DCR vs control for , sum per ' + str(time) + ' hours')
-    ax[0].legend()
-    ax[1].legend()
-    ax[2].legend()
-    ax[3].legend()
-    ax[0].set_ylabel('wake')
-    ax[1].set_ylabel('rem')
-    ax[2].set_ylabel('nrem')
-    ax[3].set_ylabel('cata')
-
-    plt.show()
+#
+# def plot_compare_sleep_state_accross_time(control ='Control', test ='DCR_HCRT'):
+#     # data_dir = 'C:/Users/maxime.juventin/Desktop/scripts_ML/data/'
+#     path_control = precompute_dir +  '/' +control+'/sleep_by_epoch.nc'
+#     path_cre = precompute_dir + '/'+ test+'/sleep_by_epoch.nc'
+#     ds_control = xr.open_dataset(path_control)
+#     ds_cre = xr.open_dataset(path_cre)
+#
+#     number_of_epochs = ds_cre['wake_all_mice'].shape[0]
+#     epoch_duration = 4
+#     duration_in_hours = int(number_of_epochs*epoch_duration/3600)
+#     wake_control = ds_control['wake_all_mice'].to_pandas()
+#     nrem_control = ds_control['nrem_all_mice'].to_pandas()
+#     rem_control = ds_control['rem_all_mice'].to_pandas()
+#     cata_control = ds_control['cata_all_mice'].to_pandas()
+#
+#     wake_cre = ds_cre['wake_all_mice'].to_pandas()
+#     nrem_cre = ds_cre['nrem_all_mice'].to_pandas()
+#     rem_cre = ds_cre['rem_all_mice'].to_pandas()
+#     cata_cre = ds_cre['cata_all_mice'].to_pandas()
+#
+#     time = int(1) # window to look at in HOUR. CAUTION must be a multiple of 100
+#     window = int(time * 3600 / epoch_duration)
+#     wake_by_time_control = []
+#     rem_by_time_control = []
+#     nrem_by_time_control = []
+#     cata_by_time_control = []
+#
+#     wake_by_time_cre = []
+#     rem_by_time_cre = []
+#     nrem_by_time_cre = []
+#     cata_by_time_cre = []
+#
+#     for h, i in enumerate(np.arange(0, number_of_epochs, window)):
+#
+#         wake_by_time_control.append(wake_control[i : i+window].sum())
+#         rem_by_time_control.append(rem_control[i : i+window].sum())
+#         nrem_by_time_control.append(nrem_control[i : i+window].sum())
+#         cata_by_time_control.append(cata_control[i : i+window].sum())
+#
+#         wake_by_time_cre.append(wake_cre[i : i+window].sum())
+#         rem_by_time_cre.append(rem_cre[i : i+window].sum())
+#         nrem_by_time_cre.append(nrem_cre[i : i+window].sum())
+#         cata_by_time_cre.append(cata_cre[i : i+window].sum())
+#     wake_by_time_control = pd.concat(wake_by_time_control, axis = 1)
+#     cata_by_time_control = pd.concat(cata_by_time_control, axis = 1)
+#     rem_by_time_control = pd.concat(rem_by_time_control, axis = 1)
+#     nrem_by_time_control = pd.concat(nrem_by_time_control, axis = 1)
+#     wake_by_time_cre = pd.concat(wake_by_time_cre, axis = 1)
+#     rem_by_time_cre = pd.concat(rem_by_time_cre, axis = 1)
+#     nrem_by_time_cre = pd.concat(nrem_by_time_cre, axis = 1)
+#     cata_by_time_cre = pd.concat(cata_by_time_cre, axis = 1)
+#
+#     # print(wake_by_time.shape)
+#     fig, ax = plt.subplots(nrows=3)
+#     times = np.arange(0, duration_in_hours)
+#     plot  = np.arange(3)
+#     controls = [wake_by_time_control, rem_by_time_control, nrem_by_time_control, cata_by_time_control]
+#     cres = [wake_by_time_cre, rem_by_time_cre, nrem_by_time_cre, cata_by_time_cre]
+#     # colors = [ 'black', 'red', 'blue']
+#     # colors = [ 'black', 'green', 'black', 'green', 'black', 'green']
+#     labels = ['control', 'control' ,'control', 'control', 'DCR', 'DCR', 'DCR' 'DCR']
+#
+#     for i, data_control, data_cre, label in zip(plot, controls, cres,labels) :
+#         m = data_control.mean(axis=0)*epoch_duration/60
+#         s =data_control.std(axis=0)*epoch_duration/60
+#         ax[i].plot(times, m, color = 'black', label = label)
+#         ax[i].fill_between(times, m-s, m+s , color = 'black', alpha = .2,)
+#         m = data_cre.mean(axis=0)*epoch_duration/60
+#         s =data_cre.std(axis=0)*epoch_duration/60
+#         ax[i].plot(times, m, color = 'green', label = label)
+#         ax[i].fill_between(times, m-s, m+s , color = 'green', alpha = .2)
+#
+#
+#     nights = { 'dark0' : [0, 4],
+#                  'dark1' : [16, 28],
+#                  'dark2' : [40, 52],
+#                  'dark3':[64, 76],
+#                  'dark4':[88, 100]}
+#
+#     for night in nights :
+#         h1, h2 = nights[night][0], nights[night][1]
+#         ax[0].axvspan(h1, h2 , color = 'black', alpha = .3)
+#         ax[1].axvspan(h1, h2 , color = 'black', alpha = .3)
+#         ax[2].axvspan(h1, h2 , color = 'black', alpha = .3)
+#         ax[3].axvspan(h1, h2 , color = 'black', alpha = .3)
+#     height0_1, height0_2 =ax[0].get_ylim()[1]*.95, ax[0].get_ylim()[1]*1.05
+#     height1_1, height1_2 = ax[1].get_ylim()[1]*.95, ax[1].get_ylim()[1]*1.05
+#     height2_1, height2_2 = ax[2].get_ylim()[1]*.95, ax[2].get_ylim()[1]*1.05
+#     height3_1, height3_2 = ax[3].get_ylim()[1]*.95, ax[3].get_ylim()[1]*1.05
+#     for x1, x2 in zip([0,28,76], [4,52,100]):
+#         lim = 110
+#         x1 += 5
+#         x2 +=5
+#         ax[0].axhspan(height0_1, height0_2, xmin =x1/lim, xmax = x2/lim, fill = False, edgecolor = 'black', hatch = '////')
+#         ax[1].axhspan(height1_1, height1_2, xmin =x1/lim, xmax = x2/lim, fill = False, edgecolor = 'black', hatch = '////')
+#         ax[2].axhspan(height2_1, height2_2, xmin =x1/lim, xmax = x2/lim, fill = False, edgecolor = 'black', hatch = '////')
+#         ax[2].axhspan(height3_1, height3_2, xmin =x1/lim, xmax = x2/lim, fill = False, edgecolor = 'black', hatch = '////')
+#     x1 = 52 + 5
+#     x2 = 58 +5
+#     ax[0].axhspan(height0_1, height0_2, xmin =x1/lim, xmax = x2/lim, color = 'red')
+#     ax[1].axhspan(height1_1, height1_2, xmin =x1/lim, xmax = x2/lim, color = 'red')
+#     ax[2].axhspan(height2_1, height2_2, xmin =x1/lim, xmax = x2/lim, color = 'red')
+#     ax[3].axhspan(height3_1, height3_2, xmin =x1/lim, xmax = x2/lim, color = 'red')
+#         # ax[0].axhspan(i,j , color = 'black', alpha = .3)
+#         # ax[1].axhspan(i,j , color = 'black', alpha = .3)
+#         # ax[2].axhspan(i,j , color = 'black', alpha = .3)
+#     ax[0].set_title('Sleep state amount DCR vs control for , sum per ' + str(time) + ' hours')
+#     ax[0].legend()
+#     ax[1].legend()
+#     ax[2].legend()
+#     ax[3].legend()
+#     ax[0].set_ylabel('wake')
+#     ax[1].set_ylabel('rem')
+#     ax[2].set_ylabel('nrem')
+#     ax[3].set_ylabel('cata')
+#
+#     plt.show()
 
 def sleep_bouts(group):
     # B0 begins at 4h, B1 etc begins at 8
@@ -353,6 +353,8 @@ def sleep_bouts(group):
                 one_and_zeros_one_mouse = selected_data[mouse].values
                 counter = 0
                 bouts = []
+                total_state = np.sum(one_and_zeros_one_mouse)*4
+
                 for i in one_and_zeros_one_mouse:
                     if i ==0:
                         bouts.append(counter)
@@ -371,6 +373,7 @@ def sleep_bouts(group):
                 bins_sleep = [2**i for i in np.arange(2,12)]
                 #bins_sleep += [max(bouts)]
                 count, bins= np.histogram(bouts, bins = bins_sleep)
+                # all_mice_count[mouse] = 100*(count*bins[:-1])/total_state
                 all_mice_count[mouse] = count
 
             df = pd.DataFrame.from_dict(all_mice_count, orient = 'index', columns = bins[:-1] )
@@ -385,22 +388,115 @@ def sleep_bouts(group):
                 os.makedirs(output_path)
             df_mean_duration.to_excel(output_path + state + '_' + cycle + '.xlsx')
 
+def REM_sleep_latency_one_mouse(mouse):
+    min_wake_duration = 6
+    ds = xr.open_dataset(precompute_dir + '/spectrums/spectrum_scoring_{}.nc'.format(mouse))
+    times = ds['times_somno'].values/3600
+    score = ds['score'].values
+    score_behavior = score.copy()
+    ctrl = get_mice('Control')
+    dcr = get_mice('DCR-HCRT')
+    group_mice = {'Control' : ctrl, 'DCR-HCRT':dcr}
+    if mouse in group_mice['Control'] :
+        group == 'Control'
+    elif mouse in group_mice['DCR-HCRT'] :
+        group == 'DCR-HCRT'
 
+
+    halfday_times= {
+                 'light1' : [0, 12],
+                 'dark1' : [12, 24],
+                 'light2' : [24, 36],
+                 'dark2' : [36, 48],
+                 'sd' : [48, 54],
+                 'light3' : [54,60],
+                 'dark3':[60, 72],
+                 'light4':[72,84 ],
+                 'dark4':[84, 96]}
+
+    for f, n in zip(['1', '2', '3'], ['w', 'n', 'r']) :
+        score_behavior = np.where(score_behavior == f, n, score_behavior)
+
+    for halfday in halfday_times:
+        dirname = excel_dir + '/{}/REM_latency/'.format(group)
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+        filename =dirname+'REM_latency_{}_{}.xlsx'.format(group, halfday)
+        if not os.path.exists(filename):
+            df = pd.DataFrame(index = group_mice[group], columns = np.arange(5))
+        else :
+            df = pd.read_excel(filename, index_col = 0)
+
+
+        t1 = halfday_times[halfday][0]
+        t2 = halfday_times[halfday][1]
+        time_mask = (times>t1) & (times<t2)
+
+        masked_score = score_behavior[time_mask]
+        rem = score_behavior[time_mask] == 'r'
+        nrem = score_behavior[time_mask] == 'n'
+        wake = score_behavior[time_mask] == 'w'
+        cata = score_behavior[time_mask] == 'a'
+
+        # fig, ax = plt.subplots()
+        latencies = []
+        ini_rem = np.diff(rem*1)
+        pos_ini_rem = np.where(ini_rem == 1)[0]+1
+        for pos in pos_ini_rem:
+            wake_duration = 0
+            for latency, ind in enumerate(np.arange(pos-1)[::-1]):
+                s = masked_score[ind]
+                if s == 'w':
+                    wake_duration += 1
+                if s == 'w' and wake_duration >=min_wake_duration:
+                    if latency == 5:
+                        pos_ini_rem = pos_ini_rem[pos_ini_rem!=pos]
+                        break
+                    else :
+                        latency -= 5
+                        latencies.append(latency)
+                        break
+                elif s == 'n' and wake_duration < min_wake_duration:
+                    wake_duration = 0
+                elif s =='r' and wake_duration == latency:
+                    pos_ini_rem = pos_ini_rem[pos_ini_rem!=pos]
+                    break
+                elif s == 'r' and wake_duration != latency:
+                    if masked_score[ind+1] == 'w':
+                        latency -= wake_duration
+                        latencies.append(latency)
+                    else :
+                        latencies.append(latency)
+                    break
+                    # break
+
+            # ax.plot(np.arange(pos-latency, pos), np.ones(latency), color = 'black')
+        print(df.columns.to_list())
+        print(len(latencies))
+        print(latencies)
+        if len(df.columns.to_list())<len(latencies):
+            df = df.reindex(columns = np.arange(len(latencies)))
+        df.loc[mouse, np.arange(len(latencies))] = np.array(latencies)
+        df.to_excel(filename)
+
+        
+def compute_all_REM_latency():
+    mice = get_mice('Control') + get_mice('DCR-HCRT')
+    for mouse in mice :
+        print(mouse)
+        REM_sleep_latency_one_mouse(mouse)
 
 
 if __name__ == '__main__' :
+    mouse = 'B4906'
 
 
     group = "DCR-HCRT"
     #group = "Control"
 
-    # precompute_sleep_state_by_epoch(group)
-    # sleep_bouts(group)
-    # sleep_state_statistics(group)
-    # plot_sleep_state_accross_time(group)
-    # plot_compare_sleep_state_accross_time(group)
-
-    compute_all()
+    # REM_sleep_latency_one_mouse(mouse)
+    compute_all_REM_latency()
+    # compute_all()
     # sleep_bouts_all()
     # sleep_state_statistics_all()
     # plt.show()
